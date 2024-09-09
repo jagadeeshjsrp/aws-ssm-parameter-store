52504fc5c0b4fe25a5cf48827f699160ebfb9e0f
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-09 19:33:42 +0530

    Ansible roboshop

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "Parameter" {
   name  = "test.test1"
-  type  = "String"
   value = "Hello World"
+  type  = "SecureString"   # type is a argument, & SecureString is a type of parameter that we will get that from hashicorp "aws_ssm_parameter" document.
+  key_id = "cda6780d-7d23-4924-8c45-67bc70f59601"
 }
