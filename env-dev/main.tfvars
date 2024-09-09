90eb1a8afa5e69d6517e7eb88c53f2c8f671722f
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-09 19:46:09 +0530

    Ansible roboshop

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name= "dev.frontend.catalogue_url", value = "http://catalogue-dev.devjsr1.online:8080/" },
+  { name= "dev.frontend.cart_url", value = "http://cart-dev.devjsr1.online:8080/" }
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "Parameter" {
-  name  = "test.test1"
-  value = "Hello World"
-  type  = "SecureString"   # type is a argument, & SecureString is a type of parameter that we will get that from hashicorp "aws_ssm_parameter" document.
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
   key_id = "cda6780d-7d23-4924-8c45-67bc70f59601"
 }
