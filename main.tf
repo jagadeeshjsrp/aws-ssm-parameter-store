1e8bbbaaa4397d59252be99f8e694bc7badd9627
jagadeesh p <jagadeeshpc5@gmail.com>
2024-09-10 19:37:14 +0530

    Ansible roboshop

--- main.tf
+++ main.tf
@@ -5,3 +5,16 @@ resource "aws_ssm_parameter" "Parameter" {
   type  = "String"
   key_id = "cda6780d-7d23-4924-8c45-67bc70f59601"
 }
+
+## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
+
+
+
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value = var.passwords[count.index].value
+  type  = "SecureString"
+  key_id = "cda6780d-7d23-4924-8c45-67bc70f59601"
+}
